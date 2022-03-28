eeb828e8015a6f564842c3122a8c0dedd9fb0786
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-29 01:53:52 +0600

    make componemt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Component/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>React Router</h1>
+      <Home/>
     </div>
   );
 }
