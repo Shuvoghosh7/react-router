5bcb3dfbef2210f34b055700b2d4d8d30f241150
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-03-29 02:40:05 +0600

    link to page by route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Component/Home/Home';
+import Friend from './Component/Friend/Friend';
+import About from './Component/About/About';
+
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <h1>Use Routes</h1>
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Friend" element={<Friend />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
     </div>
   );
 }
